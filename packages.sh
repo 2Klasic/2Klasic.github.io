de1b86a5994a4d6679f7b7eb317711793811ac24
2Klasic <derobi@live.com>
2017-03-09 13:59:28 -0600

    update

--- packages.sh
+++ packages.sh
@@ -5,4 +5,5 @@ dpkg-deb -bZgzip projects/iSHSHit debs
 dpkg-deb -bZgzip projects/CyBackup debs
 dpkg-deb -bZgzip projects/BobbyMovie_2.3.0 debs
 dpkg-deb -bZgzip projects/BobbyMovie_3.0.1 debs
+dpkg-deb -bZgzip projects/IAPCrazy_1.6.0 debs
 dpkg-deb -b Package3
