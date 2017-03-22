a788ca1d654914cbfe49e5a9cfe1fb8f3913848e
2Klasic <derobi@live.com>
2017-03-21 21:20:33 -0500

    3.0.4BM

--- packages.sh
+++ packages.sh
@@ -5,6 +5,7 @@ dpkg-deb -bZgzip projects/iSHSHit debs
 dpkg-deb -bZgzip projects/CyBackup debs
 dpkg-deb -bZgzip projects/BobbyMovie_2.3.0 debs
 dpkg-deb -bZgzip projects/BobbyMovie_3.0.1 debs
+dpkg-deb -bZgzip projects/BobbyMovie_3.0.4 debs
 dpkg-deb -bZgzip projects/IAPCrazy_1.6.0 debs
 dpkg-deb -bZgzip projects/AeroTV_1.0 debs
 dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
