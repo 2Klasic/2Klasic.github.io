7002d46a84436ed1e5eb460bbeac03038b26c1b6
derobi <Rocko@2Klasic-MBP.local>
2017-06-26 19:38:43 -0500

    update PKG

--- packages.sh
+++ packages.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/flex debs
-dpkg-deb -bZgzip projects/xCon debs
 dpkg-deb -bZgzip projects/iSHSHit debs
 #dpkg-deb -bZgzip projects/Old_CyBackup debs
 dpkg-deb -bZgzip projects/BobbyMovie_2.3.0 debs
 dpkg-deb -bZgzip projects/BobbyMovie_3.0.1 debs
 dpkg-deb -bZgzip projects/BobbyMovie_3.0.4 debs
-dpkg-deb -bZgzip projects/IAPCrazy_1.6.0 debs
 dpkg-deb -bZgzip projects/AeroTV_1.0 debs
 dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
 dpkg-deb -bZgzip projects/iCapture10 debs
@@ -18,4 +15,6 @@ dpkg-deb -bZgzip projects/Savethemblobs debs
 dpkg-deb -bZgzip projects/mterminal_1.4-5 debs
 dpkg-deb -bZgzip projects/cybackup10_0.0.3 debs
 dpkg-deb -bZgzip projects/iOS11_System debs
+dpkg-deb -bZgzip projects/setuptools debs
+dpkg-deb -bZgzip projects/sbutils debs
 dpkg-deb -b Package3
