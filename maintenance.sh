df3cc1ff6ec9d48e76b2639b6bdfa0db0b3cdb4b
derobi <Rocko@2Klasic-MBP.local>
2018-07-18 20:19:00 -0500

    test_update

--- maintenance.sh
+++ maintenance.sh
@@ -10,14 +10,14 @@ rm -r Packages.bz2
 echo [+] Building Debs..
 dpkg-deb -bZgzip projects/iSHSHit debs
 #dpkg-deb -bZgzip projects/Old_CyBackup debs
-dpkg-deb -bZgzip projects/MovieDB debs
+#dpkg-deb -bZgzip projects/MovieDB debs
 #dpkg-deb -bZgzip projects/Satiné debs
 #dpkg-deb -bZgzip projects/BobbyMovie_2.3.0 debs
 #dpkg-deb -bZgzip projects/BobbyMovie_3.0.1 debs
 #dpkg-deb -bZgzip projects/BobbyMovie_3.0.4 debs
 #dpkg-deb -bZgzip projects/AeroTV_1.0 debs
-#dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
-dpkg-deb -bZgzip projects/iCapture10 debs
+dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
+#dpkg-deb -bZgzip projects/iCapture10 debs
 #dpkg-deb -bZgzip projects/Pandolife debs
 dpkg-deb -bZgzip projects/extendlife debs
 dpkg-deb -bZgzip projects/CyBackup_0.6 debs
