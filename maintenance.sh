ca8cff422cf51cdbb0de9140960b1aa222c2ad8e
derobi <Rocko@2Klasic-MBP.local>
2018-07-25 20:00:08 -0500

    res

--- maintenance.sh
+++ maintenance.sh
@@ -8,7 +8,7 @@ find debs -type f -name '*.deb' -delete
 rm -r Packages.bz2
 
 echo [+] Building Debs..
-dpkg-deb -bZgzip projects/iSHSHit debs
+#dpkg-deb -bZgzip projects/iSHSHit debs
 dpkg-deb -bZgzip projects/drops_of_jupitor debs
 #dpkg-deb -bZgzip projects/Old_CyBackup debs
 #dpkg-deb -bZgzip projects/MovieDB debs
@@ -17,18 +17,18 @@ dpkg-deb -bZgzip projects/drops_of_jupitor debs
 #dpkg-deb -bZgzip projects/BobbyMovie_3.0.1 debs
 #dpkg-deb -bZgzip projects/BobbyMovie_3.0.4 debs
 #dpkg-deb -bZgzip projects/AeroTV_1.0 debs
-dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
+#dpkg-deb -bZgzip projects/AeroTV_1.0b8 debs
 #dpkg-deb -bZgzip projects/iCapture10 debs
 #dpkg-deb -bZgzip projects/Pandolife debs
-dpkg-deb -bZgzip projects/extendlife debs
-dpkg-deb -bZgzip projects/CyBackup_0.6 debs
-dpkg-deb -bZgzip projects/iUtility4iOS debs
-dpkg-deb -bZgzip projects/Savethemblobs debs
-dpkg-deb -bZgzip projects/mterminal_1.4-5 debs
-dpkg-deb -bZgzip projects/cybackup10_0.0.3 debs
-dpkg-deb -bZgzip projects/iOS11_System debs
-dpkg-deb -bZgzip projects/setuptools debs
-dpkg-deb -bZgzip projects/sbutils debs
+#dpkg-deb -bZgzip projects/extendlife debs
+#dpkg-deb -bZgzip projects/CyBackup_0.6 debs
+#dpkg-deb -bZgzip projects/iUtility4iOS debs
+#dpkg-deb -bZgzip projects/Savethemblobs debs
+#dpkg-deb -bZgzip projects/mterminal_1.4-5 debs
+#dpkg-deb -bZgzip projects/cybackup10_0.0.3 debs
+#dpkg-deb -bZgzip projects/iOS11_System debs
+#dpkg-deb -bZgzip projects/setuptools debs
+#dpkg-deb -bZgzip projects/sbutils debs
 dpkg-deb -bZgzip projects/xpwn debs
 dpkg-deb -bZgzip projects/kdfu debs
 dpkg-deb -bZgzip projects/kloader debs
